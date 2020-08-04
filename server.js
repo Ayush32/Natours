643e63c057394f327e4e47cc8752225d86973b21
Ayush Gupta <721ayush@gmail.com>
2020-08-04 18:53:56 +0530

    creating first schema Tour

--- server.js
+++ server.js
@@ -22,6 +22,25 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}..`);

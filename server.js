f648c4b1dd2fb7437fa5a99c8ad3555528dd9ccd
Ayush Gupta <721ayush@gmail.com>
2020-08-04 18:36:28 +0530

    setting database mongoose

--- server.js
+++ server.js
@@ -2,9 +2,25 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

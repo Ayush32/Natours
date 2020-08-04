58402ff3241b6f6c18307295900921a14cf924e2
Ayush Gupta <721ayush@gmail.com>
2020-08-04 22:54:20 +0530

    update tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -82,12 +82,13 @@ exports.createTour = async (req, res) => {
 exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
       runValidators: true,
     });
     res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour here....>',
+        tour,
       },
     });
   } catch (err) {

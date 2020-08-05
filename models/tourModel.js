849090f35bf4f35d107c0f8410244102abe9d98a
Ayush Gupta <721ayush@gmail.com>
2020-08-05 19:10:57 +0530

    modelling tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -11,10 +11,30 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a price'],
     unique: true,
   },
-  rating: {
+
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],

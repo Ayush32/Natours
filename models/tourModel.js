cd4cc8cf3db43173f209264900207ae8d32b476c
Ayush Gupta <721ayush@gmail.com>
2020-08-05 19:27:11 +0530

    modelling tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,6 +10,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a price'],
     unique: true,
+    trim: true,
   },
 
   duration: {
@@ -39,6 +40,30 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);

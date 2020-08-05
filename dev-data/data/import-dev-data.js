f7b2b5d140a1eaf5cf15277e02b19531973de190
Ayush Gupta <721ayush@gmail.com>
2020-08-05 20:17:42 +0530

    completed filetring the data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,8 +2,10 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
+const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -21,3 +23,41 @@ mongoose
     // console.log(con.connections);
     console.log('DB connection successful!');
   });
+
+// Read the file
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into the database
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//  Delete all data from DB COLLECTION
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data Successfully Deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);

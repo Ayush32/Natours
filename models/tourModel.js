f63ec7d4ca88cc7012565d674b92a4798eed7025
Ayush Gupta <721ayush@gmail.com>
2020-08-08 19:09:44 +0530

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,6 +4,7 @@
  */
 
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -13,6 +14,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
 
     duration: {
       type: Number,
@@ -79,6 +81,16 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+tourSchema.pre('save', function () {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.post('save', function (soc, next) {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);

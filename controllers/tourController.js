77b58af48d5f58f09454e82d3cccd58a42f063b4
Ayush Gupta <721ayush@gmail.com>
2020-08-08 18:33:32 +0530

    aggreation completed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -223,6 +223,24 @@ exports.getMonthlyPlan = async (req, res) => {
           },
         },
       },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      {
+        $limit: 12,
+      },
     ]);
     res.status(200).json({
       status: 'success',

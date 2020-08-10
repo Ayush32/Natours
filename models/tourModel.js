77a073282ecfe4eef9f25304f3793d05372f21d4
Ayush Gupta <721ayush@gmail.com>
2020-08-10 19:39:38 +0530

    not using validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -18,7 +18,7 @@ const tourSchema = new mongoose.Schema({
         'A tour name must have less or equal than 40 characters',
       ],
       minlength: [10, 'A tour name must have more or equal than 40 characters'],
-      validate: validator.isAlpha
+      validate: [validator.isAlpha, 'Tour name must only contain character'],
     },
     slug: String,
 

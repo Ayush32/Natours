54065b8c26dc7c89f3e71f8cc92689c95b5cc8ae
Ayush Gupta <721ayush@gmail.com>
2020-08-10 20:45:56 +0530

    global error handling

--- app.js
+++ app.js
@@ -48,12 +48,14 @@ app.all('*', (req, res, next) => {
   err.status = 'fail'
   err.statusCode = 404;
 
-  next();
-})
+  next(err);
+});
+
+
 
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error'
+  err.status = err.status || 'error';
 
   res.status(err.statusCode).json({
     status: err.status,

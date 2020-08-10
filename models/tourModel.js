0a6663e693f3d4937b91a28403378ce93a88f385
Ayush Gupta <721ayush@gmail.com>
2020-08-10 19:21:44 +0530

    data valiadators

--- models/tourModel.js
+++ models/tourModel.js
@@ -16,7 +16,7 @@ const tourSchema = new mongoose.Schema({
         40,
         'A tour name must have less or equal than 40 characters',
       ],
-      minlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 40 characters'],
     },
     slug: String,
 
@@ -32,10 +32,17 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
 
     ratingsQuantity: {

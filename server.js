5cf643918e7a00751f36b1191092e14eddc88661
Ayush Gupta <721ayush@gmail.com>
2020-08-12 21:14:39 +0530

    complete error hand;e

--- server.js
+++ server.js
@@ -5,6 +5,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION SHUTTING DOWN.....");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 

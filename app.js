e7911a1f89f9900d07836e674d957167482f2179
Ayush Gupta <721ayush@gmail.com>
2020-08-12 17:21:30 +0530

    error refactor

--- app.js
+++ app.js
@@ -2,18 +2,16 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
-const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const {
-  static
-} = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const { static } = require("express");
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
@@ -36,31 +34,29 @@ app.use((req, res, next) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 // writing the ROUTES in better way
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.all('*', (req, res, next) => {
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.all("*", (req, res, next) => {
   // res.status(404).json({
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server!`
   // })
 
   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail'
+  err.status = "fail";
   err.statusCode = 404;
 
   next(err);
 });
 
-
-
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
 
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
-  })
-})
+    message: err.message,
+  });
+});
 
 module.exports = app;
---
+++ utils/appError.js
@@ -0,0 +1,6 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+
+class AppError extends Error {}

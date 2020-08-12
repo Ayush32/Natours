22ed652b1118c6aa50cad83df0b22292807a1a42
Ayush Gupta <721ayush@gmail.com>
2020-08-12 18:08:27 +0530

    global error handler

--- app.js
+++ app.js
@@ -4,9 +4,12 @@
  */
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const { static } = require("express");
+const { Error } = require("mongoose");
 
 const app = express();
 
@@ -36,27 +39,11 @@ app.use((req, res, next) => {
 // writing the ROUTES in better way
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`
-  // })
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};

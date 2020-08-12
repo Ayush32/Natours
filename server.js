d2775a41595a01d0b962769b6b4054d88a56cb08
Ayush Gupta <721ayush@gmail.com>
2020-08-12 21:06:05 +0530

    hnadled all rejection

--- server.js
+++ server.js
@@ -21,10 +21,17 @@ mongoose
   .then(() =>
     // console.log(con.connections);
     console.log("DB connection successful!")
-  )
-  .catch((err) => console.log("ERROR"));
+  );
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}..`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION SHUTTING DOWN.....");
+  server.close(() => {
+    process.exit(1);
+  });
 });

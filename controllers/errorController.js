ccee82f730e6016c99e126298e1458809f9a4c60
Ayush Gupta <721ayush@gmail.com>
2020-08-12 20:18:45 +0530

    duplicate error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,9 +11,10 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   console.log(value);
-  const message = "Duplicate field value: x. Please use another value";
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
 };
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({

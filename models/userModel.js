6c9dd9c231d2679d708af5449989c4719518f5ce
Ayush Gupta <721ayush@gmail.com>
2020-08-13 12:29:38 +0530

    update user schema

--- models/userModel.js
+++ models/userModel.js
@@ -3,32 +3,33 @@
  *   All rights reserved.
  */
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 // name email, photo, password, cnfrim pass
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'Please tell us your name!']
+    require: [true, "Please tell us your name!"],
   },
   email: {
     type: String,
-    require: [true, 'Please provide your email'],
+    require: [true, "Please provide your email"],
     unique: true,
     lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
-  photo: [String],
+  photo: String,
 
   password: {
-      type: String,
-      required: true
-  }
-  confirmPassword : {
-      type: String,
-      require: true
-
-  }
+    type: String,
+    required: [true, "Please provide a password!"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    require: [true, "Please confirm your password"],
+  },
 });
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;

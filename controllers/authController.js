100a0bf4289d230b37eb17dcf7b5ea783d9f16ea
Ayush Gupta <721ayush@gmail.com>
2020-08-14 21:02:23 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,5 +104,11 @@ exports.protect = catchAsync(async (req, res, next) => {
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles is an array
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
   };
 };

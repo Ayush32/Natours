beda9eb5c500af4d254cf0e71f0b50201f601ce5
Ayush Gupta <721ayush@gmail.com>
2020-08-15 11:48:22 +0530

    completing nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -129,6 +129,18 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetURL = `${req.protocol}://${req.get(
     "host"
   )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did'nt forget your password, please ignore this email`;
+
+  await sendEmail({
+    email: user.email,
+    subject: "Your password reset token (valid for 10 min)",
+    message,
+  });
+  res.status(200).json({
+    status: "success",
+    message: "Token sent to email",
+  });
 });
 
 exports.resetPassword = (req, res, next) => {};

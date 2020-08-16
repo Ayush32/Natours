83cada551103ca4c85029de29796d305a88573e7
Ayush Gupta <721ayush@gmail.com>
2020-08-16 12:42:49 +0530

    install mongo-sanitization

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use("/api", limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
-// serving static files
+
+// serving static files XSS
 app.use(express.static(`${__dirname}/public`));
 
 // 1st middleware

f09f68f4f8d7646ba33c56625f23765bb2a34ce3
Ayush Gupta <721ayush@gmail.com>
2020-08-16 08:39:11 +0530

    update password

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,14 @@ const catchAsync = require("./../utils/catchAsync");
 const User = require("../models/userModel");
 const AppError = require("./../utils/appError");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
   res.status(500).json({
@@ -27,7 +35,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   // 2)- update user document
-  const updateUser = await User.findByIdAndUpdate(req.user.id, x, {
+  const filterBody = filterObj(req.body, "name", "email");
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
     new: true,
     runValidators: true,
   });

b3daf1174c6cb4a81bf839e9b471956d6ff579dc
Ayush Gupta <721ayush@gmail.com>
2020-08-16 07:57:06 +0530

    update password

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -24,15 +35,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: "success",
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -181,4 +184,22 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     status: "success",
     token,
   });
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select("+password");
+
+  // 2) Check if posted current password is correct
+  if (!(await user.correctPassword(req.body.passwordConfirm, user.password))) {
+    return next(new AppError("Your current password is wrong", 401));
+  }
+
+  // 3)  if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // user.findbyIDandupdate
+
+  // 4) Log user id send JWT
 });

95f5387bb44e1c7562be6409c02cc52c61885997
Ayush Gupta <721ayush@gmail.com>
2020-08-16 11:40:19 +0530

    delete the user

--- models/userModel.js
+++ models/userModel.js
@@ -48,6 +48,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre("save", async function (next) {

9a36c5ea6890976a1d0cbb6f2403194ac9687f42
Ayush Gupta <721ayush@gmail.com>
2020-08-16 12:24:46 +0530

    secure the  req

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const rateLimit = require("express-rate-limit");
 const { static } = require("express");
 const { Error } = require("mongoose");
 
 const app = express();
-
+// 1) global middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60* 1000
+})
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));

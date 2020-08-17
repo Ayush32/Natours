6bdae82d07c8395d0d1281994eb8a7f4a30122c3
Ayush Gupta <721ayush@gmail.com>
2020-08-17 11:40:28 +0530

    refrencing guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 const validator = require("validator");
-
+const User = require("./userModel");
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -113,6 +113,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
 
   {
@@ -129,10 +130,16 @@ tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
 
-// tourSchema.pre('save', function () {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
+tourSchema.pre("save", function () {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre("save", async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document....');

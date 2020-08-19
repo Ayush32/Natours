4fb75ca93d334c2f11361f36234b369a2cb38abd
Ayush Gupta <721ayush@gmail.com>
2020-08-19 08:36:24 +0530

    creating review schema

---
+++ models/reviewModel.js
@@ -0,0 +1,48 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+// review /rating / createDaT/ REF TO TOUR / ref to user
+
+const mongoose = require("mongoose");
+const { model } = require("./userModel");
+const User = require("./userModel");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review cannot be empty"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong t a tour"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must be belong to a user"],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;

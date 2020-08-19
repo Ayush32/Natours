074809c8712069b16f09d3b5cef8ca8bad3df384
Ayush Gupta <721ayush@gmail.com>
2020-08-19 20:58:09 +0530

    complete nested tour

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,7 +6,7 @@
 const express = require("express");
 const reviewController = require("./../controllers/reviewController");
 const authController = require("./../controllers/authController");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route("/")

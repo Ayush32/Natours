7c8a6ccd2bb20b124b12c56715b95276b9fbf6ee
Ayush Gupta <721ayush@gmail.com>
2020-08-20 13:16:39 +0530

    importing data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Tour = require("./../../models/tourModel");
+const Review = require("./../../models/reviewModel");
+const User = require("./../../models/userModel");
 
 dotenv.config({ path: "./config.env" });
 
@@ -27,6 +29,8 @@ mongoose
 // Read the file
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 
 // import data into the database
 

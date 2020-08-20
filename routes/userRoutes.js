9832edd0158f12c8fd2108afd4851c63a1788d07
Ayush Gupta <721ayush@gmail.com>
2020-08-20 12:58:37 +0530

    portect user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,8 @@ const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 

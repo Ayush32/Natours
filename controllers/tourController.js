2c8e60f5be8eaf47c37ef84d192a32b7b9f816e9
Ayush Gupta <721ayush@gmail.com>
2020-08-20 12:17:33 +0530

    factory populate

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,21 +56,7 @@ exports.getAllTours = catchAsync(async (req, res) => {
     },
   });
 });
-exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate("Reviews");
-  // Tour.findOne({_id: req.params.id})
-  if (!tour) {
-    return next(new AppError("No tour find with this ID", 404));
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-});
-
-exports.createTour = factory.createOne(Tour);
+exports.getTour = exports.createTour = factory.createOne(Tour);
 
 exports.updateTour = factory.updateOne(Tour);
 

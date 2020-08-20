86fe9385f3b31e4e6020cfe9129b5c72e0e7486b
Ayush Gupta <721ayush@gmail.com>
2020-08-20 12:46:16 +0530

    get current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 // update the user
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1)- Create error if user POSTs password data

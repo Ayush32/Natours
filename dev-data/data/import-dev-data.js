0a75d3391cdea12b486eeae805a14b9c57ccf00b
Ayush Gupta <721ayush@gmail.com>
2020-08-20 13:21:26 +0530

    delete data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -29,14 +29,18 @@ mongoose
 // Read the file
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
-const reviews = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // import data into the database
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users);
+    await Review.create(reviews);
     console.log("Data Successfully loaded");
   } catch (err) {
     console.log(err);
@@ -49,6 +53,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data Successfully Deleted!");
   } catch (err) {
     console.log(err);

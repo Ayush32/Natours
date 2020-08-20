b7826146736bb16c77fcea50e1a3819f69aad68a
Ayush Gupta <721ayush@gmail.com>
2020-08-20 12:34:45 +0530

    get all user using factory

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,16 +18,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-  res.status(500).json({
-    status: "success",
-    data: {
-      users,
-    },
-  });
-});
-
 // update the user
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1)- Create error if user POSTs password data
@@ -61,14 +51,17 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = factory.getOne(User);
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: "error",
     message: "This route is not defined! Please use sign up instead",
   });
 };
+
+exports.getAllUsers = factory.getAll(User);
+
+exports.getUser = factory.getOne(User);
+
 // don not change assword
 // update user
 exports.updateUser = factory.updateOne(User);

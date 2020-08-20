df61c295c9c4a051b517772f22a0e0f92f10672d
Ayush Gupta <721ayush@gmail.com>
2020-08-20 13:08:27 +0530

    protect review

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,11 +8,12 @@ const reviewController = require("./../controllers/reviewController");
 const authController = require("./../controllers/authController");
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route("/")
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo("user"),
     reviewController.setTourUserIds,
     reviewController.createReview

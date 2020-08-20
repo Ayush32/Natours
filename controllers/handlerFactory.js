b680a7ccc0a9ee02024bc2fad4e758fafb2ff61d
Ayush Gupta <721ayush@gmail.com>
2020-08-20 12:31:26 +0530

    get all reviews with factory handle

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -67,7 +67,11 @@ exports.getOne = (Model, popOptions) =>
 
 exports.getAll = (Model) =>
   catchAsync(async (req, res) => {
-    const features = new APIFeatures(Model.find(), req.query)
+    // to allow nested GET Review
+    let = filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -7,27 +7,15 @@ const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 const factory = require("./../controllers/handlerFactory");
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let = filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: "success",
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
   next();
 };
+
+exports.getAllReviews = factory.getAll(Review);
+
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);

89260ee770dc8c0e3fd45c613e2f601e06c657cd
Ayush Gupta <721ayush@gmail.com>
2020-08-21 18:42:27 +0530

    comments in pug

--- app.js
+++ app.js
@@ -81,7 +81,10 @@ app.use((req, res, next) => {
 
 // writing the ROUTES in better way
 app.get("/", (req, res) => {
-  res.status(200).render("base");
+  res.status(200).render("base", {
+    tour: "The Forest Hiker",
+    user: "Ayush",
+  });
 });
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);

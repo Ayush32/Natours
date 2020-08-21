fc56df9752d5edebeb8f1fc8477ed910503108f2
Ayush Gupta <721ayush@gmail.com>
2020-08-21 22:40:57 +0530

    showing content

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,11 +2,21 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get the tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1
   res.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("tour", {

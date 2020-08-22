6d949dcc825db314d3338d7df1bc576c5a016204
Ayush Gupta <721ayush@gmail.com>
2020-08-22 07:30:19 +0530

    building tour template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -18,8 +18,16 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1 get the data fro the requested tour including review and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "review",
+    fields: "review rating user",
+  });
+
+  // 2 build template
   res.status(200).render("tour", {
     title: "The Forest Hiker",
+    tour,
   });
-};
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,6 +8,6 @@ const viewController = require("./../controllers/viewsController");
 const router = express.Router();
 
 router.get("/", viewController.getOverview);
-router.get("/tour", viewController.getTour);
+router.get("/tour/:id/:slug", viewController.getTour);
 
 module.exports = router;
